c24ef8e73ea4cca907f3805355017d0363c69177
Ismael Cuevas <ijcuevas2@gmail.com>
2026-06-07 23:58:21 -0500

    updating latex notes

---
+++ .auctex-auto/system_design_interview.el
@@ -0,0 +1,44 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "system_design_interview"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("report" "")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("listings" "") ("amsthm" "") ("amsmath" "") ("tcolorbox" "") ("calligra" "") ("siunitx" "") ("url" "") ("float" "") ("caption" "labelformat=empty") ("tikz" "") ("graphicx" "") ("parskip" "")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "report"
+    "rep10"
+    "listings"
+    "amsthm"
+    "amsmath"
+    "tcolorbox"
+    "calligra"
+    "siunitx"
+    "url"
+    "float"
+    "caption"
+    "tikz"
+    "graphicx"
+    "parskip")
+   (TeX-add-symbols
+    "oldsection"
+    "oldsubsection"
+    "oldsubsubsection"
+    "oldparagraph"
+    "oldsubparagraph")
+   (LaTeX-add-labels
+    "tab:label")
+   (LaTeX-add-environments
+    '("defbox" 1)))
+ :latex)
+
