9f970036936fba4c320ed32f397ae64bf73a5fb6
Ismael Cuevas <ijcuevas2@gmail.com>
2026-07-31 11:54:12 -0500

    updating changes

--- .auctex-auto/system_design_interview.el
+++ .auctex-auto/system_design_interview.el
@@ -8,12 +8,12 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("listings" "") ("amsthm" "") ("amsmath" "") ("tcolorbox" "") ("calligra" "") ("siunitx" "") ("url" "") ("float" "") ("caption" "labelformat=empty") ("varwidth" "") ("tikz" "") ("graphicx" "") ("parskip" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "report"
